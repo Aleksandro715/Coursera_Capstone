7ff5dfaf1875988a6d0eeb14a2fc76f34cedca7c
Aleksandro Vogli <vogli.aleksandro@gmail.com>
2020-05-30 12:26:27 +0200

    project1

---
+++ python_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <p> this repository is going tobe used mainly for capstone project</p>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print (" Hello Capstone Project Course!")
